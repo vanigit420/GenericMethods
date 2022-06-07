95400743c16d17e97c9ab387a0ab8485356e2bc3
shivani korwan <shivanikorwan63232@gmail.com>
2022-06-07 09:30:41 +0530

    find max string

--- Generic/Generic/Program.cs
+++ Generic/Generic/Program.cs
@@ -15,7 +15,8 @@ namespace Generic
 
             FindMaximum findMaximum = new FindMaximum();
             //Console.WriteLine(findMaximum.MaximumIntegerNum(98, 150, 88));
-            Console.WriteLine(findMaximum.MaximumFloatNum(5.678, 8.997, 12.145));
+            //Console.WriteLine(findMaximum.MaximumFloatNum(5.678, 8.997, 12.145));
+            Console.WriteLine(findMaximum.MaximumString("50", "40", "35"));
 
 
         }
